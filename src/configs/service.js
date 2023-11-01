a42b6bcfc9e4eee4950ed3e2020ff7be37ea5236
Kokleng-Dev <y.koklengmedd@gmail.com>
2023-11-01 13:13:42 +0700

    axios

--- src/configs/service.js
+++ src/configs/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getPath } from "./api";
-
+// hello 
 axios.defaults.baseURL = 'https://fakestoreapi.com/api';
 axios.defaults.headers.common['Authorization'] = 'Bearer';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
